c6dcb4078a6a7f05a2d401cb5c82a251b009d86c
alpeeff <awdasgsdr@gmail.com>
2025-01-30 18:46:23 +0200

    feat: minimize size

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import preact from "@preact/preset-vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [preact(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
